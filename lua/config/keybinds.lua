cfa959c1ddc4c7b389a55e6e303bcbd72087b9b9
yao <yaoduanmu3@gmail.com>
2026-03-12 16:29:22 -0400

    window keymaps

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -2,4 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", ":write<CR>")
 vim.keymap.set("n", "<leader>q", ":quit<CR>")
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 -- vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
