f0ab5f40ee524d279815b0c6d8ac5cd8f6022177
yao <yaoduanmu3@gmail.com>
2026-03-12 19:21:46 -0400

    treesitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 		lazy = false,
 		build = ':TSUpdate',
 		config = function()
-			require('nvim-treesitter.configs').setup({
+			require('nvim-treesitter.config').setup({
 				highlight = {
 					enable = true,
 					additional_vim_regex_highlighting = false,
