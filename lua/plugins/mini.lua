743fd8c144ffdfdf8c8878df48f17bee0c19240e
yao <yaoduanmu3@gmail.com>
2026-03-12 16:18:54 -0400

    mini.pick config fix

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,9 +3,7 @@ return {
 		"echasnovski/mini.nvim",
 		version = false,
 		config = function()
-			require("mini.pick").setup({
-				mappings = {close = "q"}
-			})
+			require("mini.pick").setup()
 			require("mini.pairs").setup()
 			require("mini.files").setup()
 			vim.keymap.set("n", "<leader>e", require("mini.files").open, { desc = "Open Mini Files" })
