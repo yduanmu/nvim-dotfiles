e7a92c7514aeb16d6d558c2ef6b63e8d727f93ea
yao <yaoduanmu3@gmail.com>
2026-03-12 19:25:02 -0400

    keybind fix

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,6 +9,9 @@ return {
 			-- require("mini.icons").setup() -- mini-completion dependency
 			require("mini.snippets").setup() -- mini-completion dependency
 			require("mini.completion").setup()
+
+			vim.keymap.set("n", "<leader>e", require("mini.files").open, { desc = "Open Mini Files" })
+			vim.keymap.set("n", "<Esc>", require("mini.files").close, { desc = "Close Mini Files" })
 		end,
 	},
 }
