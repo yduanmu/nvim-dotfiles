daad28825bc0de3767f0e2bf72ad30395ad34e0d
yao <yaoduanmu3@gmail.com>
2026-03-12 23:17:38 -0400

    nvim mini source update

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require('config.keybinds')
 require('config.lazy')
 require('lsp.clangd')
 require('lsp.luals')
+require('plugins.treesitter')
 
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
