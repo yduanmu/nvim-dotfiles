5ada3ccdcdb48534da53ae1f5b66d666520532f2
yao <yaoduanmu3@gmail.com>
2026-03-13 20:26:12 -0400

    mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,6 +10,7 @@ return {
 			-- require("mini.icons").setup() -- mini-completion dependency
 			require("mini.snippets").setup() -- mini-completion dependency
 			require("mini.completion").setup()
+			require("mini.surround").setup()
 			
 			vim.keymap.set("n", "<leader>e", require("mini.files").open, { desc = "Open Mini Files" })
 			vim.keymap.set("n", "<Esc>", require("mini.files").close, { desc = "Close Mini Files" })
