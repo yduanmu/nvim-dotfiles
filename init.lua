d33b0c67ae2f7f2e10e0ec0811fe7cd47edd6ca1
yao <yaoduanmu3@gmail.com>
2026-08-21 11:03:39 -0600

    remove osc52

--- init.lua
+++ init.lua
@@ -9,7 +9,6 @@ require('lsp.luals')
 require('lsp.pyright')
 require('lsp.texlab')
 
-vim.g.clipboard = 'osc52'
 vim.g.vimtex_view_method = 'skim'
 vim.g.vimtex_view_skim_sync = 1
 vim.g.vimtex_view_skim_activate = 1
