f87bc1896b2ef77e7d63761338627b39e14fc159
yao <yaoduanmu3@gmail.com>
2026-08-25 15:07:22 -0600

    query driver for moons

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -1,5 +1,5 @@
 vim.lsp.config('clangd', {
-	cmd = { 'clangd', '--background-index'}, -- opt heavy: --clang-tidy, --completion-style=detailed
+	cmd = { 'clangd', '--background-index', '--query-driver=/usr/bin/c++,/usr/bin/g++,/usr/bin/clang++'}, -- opt heavy: --clang-tidy, --completion-style=detailed
 	filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
 	root_markers = { '.git', 'compile_commands.json' },
 	autostart = false,
